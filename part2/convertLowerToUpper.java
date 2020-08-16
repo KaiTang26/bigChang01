a6847eb9f2e19f0b2ea496f8acacdeb9c080c85f
Kai Tang <16kt16@queensu.ca>
2020-08-16 18:14:03 -0400

    leap year

---
+++ part2/convertLowerToUpper.java
@@ -0,0 +1,9 @@
+class Solution {
+    public static char convertLowerToUpper(char input){
+        char result = (char) (input-'a'+'A');
+        return result;
+    }
+    
+}
+
+// 1: convert int to char
