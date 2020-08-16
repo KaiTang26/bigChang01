98341f53f24f12e6bb2a1db7ed48b40397347912
Kai Tang <16kt16@queensu.ca>
2020-08-16 17:38:25 -0400

    exchangeTwoInt

---
+++ part1/exchangeTwoInt.java
@@ -0,0 +1,15 @@
+class Solution {
+    public static void exchangeTwoInt(int a, int b){
+
+        System.out.println("before a: "+ a);
+        System.out.println("before b: "+ b);
+
+        int c = a;
+        a = b;
+        b = c;
+        System.out.println("after a: "+ a);
+        System.out.println("after b: "+ b);
+
+    }
+    
+}
