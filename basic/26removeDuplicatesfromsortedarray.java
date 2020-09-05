06070dbd22f4c1d906091b76fe842a0db363526c
Kai Tang <16kt16@queensu.ca>
2020-09-05 10:30:01 -0400

    80

---
+++ basic/26removeDuplicatesfromsortedarray.java
@@ -0,0 +1,28 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+}
+
+class Solution {
+    public int removeDuplicates(int[] nums) {
+
+        int lastIndexOfReturnArray = 0;
+        for(int i=0; i<nums.length; i++){
+            if(nums[i]!=nums[lastIndexOfReturnArray]){
+                lastIndexOfReturnArray++;
+                nums[lastIndexOfReturnArray]=nums[i];
+            }
+        }
+
+        return lastIndexOfReturnArray+1;
+
+    }
+}
+
+
+// Time complexity: O(n)
+// Space complexity: O(1)
+// slow and fast pointer: slow one (new array last element), fast one
+// [0,0,1,1,1,2,2,3,3,4] => [0,1,2,3,4,2,2,3,3,4]
---
+++ basic/80RemoveDuplicatesII.java
@@ -0,0 +1,55 @@
+package com.company;
+import java.util.ArrayList; // import the ArrayList class
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int[] test = new int[]{1,1,1,2,2,2,3};
+        Solution sl = new Solution();
+
+        int ans = sl.removeDuplicates(test);
+
+        System.out.println(ans);
+
+        for (int i = 0; i < test.length; i++) {
+            System.out.println(test[i]);
+        }
+
+
+// [1,1,2,2,3]
+
+    }
+}
+
+
+class Solution {
+    public int removeDuplicates(int[] nums) {
+
+        int lastIndexOfReturnArray = 2;
+        for(int i=2; i<nums.length; i++){
+            if(nums[lastIndexOfReturnArray-2] != nums[i]){
+
+                nums[lastIndexOfReturnArray]=nums[i];
+                lastIndexOfReturnArray++;
+            }
+        }
+
+        return lastIndexOfReturnArray;
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
