bedc6661760ec8a060f175292549540792886a80
Tang, Kai <kai_tang@manulife.com>
2020-09-08 12:39:01 -0400

    383

---
+++ basic/String/383Ransom.java
@@ -0,0 +1,38 @@
+class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+
+        char[] ransomNoteArray = ransomNote.toCharArray();
+
+        char[] magazineArray = magazine.toCharArray();
+
+        Map<Character, Integer> hashMap = new HashMap<Character, Integer>();
+
+        for(int i=0; i<magazineArray.length; i++){
+            if(!hashMap.containsKey(magazineArray[i])){
+                hashMap.put(magazineArray[i], 1);
+            }else{
+                int count = hashMap.get(magazineArray[i])+1;
+                hashMap.put(magazineArray[i], count);
+            }
+        }
+
+        for(int i=0; i< ransomNoteArray.length; i++){
+            if(hashMap.containsKey(ransomNoteArray[i])){
+                if(hashMap.get(ransomNoteArray[i])<=0){
+                    return false;
+                }else{
+                    int count = hashMap.get(ransomNoteArray[i]) -1;
+                    hashMap.put(ransomNoteArray[i], count);
+                }
+
+            }else{
+                return false;
+            }
+        }
+
+
+
+        return true;
+
+    }
+}
