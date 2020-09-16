bb1f8f56be305912d71fccfb1ab0ebdf0bef1058
Tang, Kai <kai_tang@manulife.com>
2020-09-15 23:40:19 -0400

    83

---
+++ basic/LinkedList/83RemoveDuplicate.java
@@ -0,0 +1,24 @@
+class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+        if(head ==null){
+            return null;
+        }
+        ListNode slow = head;
+        ListNode fast = slow.next;
+        while(fast != null){
+            if(slow.val == fast.val){
+                while(fast != null && slow.val == fast.val){
+                    fast =fast.next;
+                }
+                slow.next = fast;
+            }else{
+
+                slow = slow.next;
+                fast = slow.next;
+
+            }
+        }
+        return head;
+
+    }
+}
